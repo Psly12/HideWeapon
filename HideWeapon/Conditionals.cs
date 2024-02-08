d8cf8f6ab0c945b95903cba565c5b873c108e0df
Neel <neel.salvi@gmail.com>
2024-02-08 23:52:17 +0000

    Add project files.

---
+++ HideWeapon/Conditionals.cs
@@ -0,0 +1,29 @@
+﻿using SharpPluginLoader.Core;
+
+
+namespace HideWeapon
+{
+    internal class Conditionals
+    {
+        public static readonly Stage[] nonCombatStages =
+        [
+           Stage.Astera,
+           Stage.AsteraHub,
+           Stage.ChamberOfFive,
+           Stage.ResearchBase,
+           Stage.Seliana,
+           Stage.SelianaHub,
+        ];
+
+        public static readonly Dictionary<string, Int32> SubWeapon = new Dictionary<string, Int32>
+        {
+            { "uWp01", 0x2358},
+            { "uWp02", 0x2350},
+            { "uWp03", 0x2350},
+            { "uWp06", 0x2358},
+            { "uWp07", 0x2350},
+            { "uWp09", 0x2350},
+            { "uWp11", 0x2368},
+        };
+    }
+}
---
+++ HideWeapon/Plugin.cs
@@ -0,0 +1,64 @@
+﻿using SharpPluginLoader.Core;
+using SharpPluginLoader.Core.Entities;
+using SharpPluginLoader.Core.Weapons;
+namespace HideWeapon
+{
+    public class Plugin : IPlugin
+    {
+        public string Name => "Hide Weapon";
+
+        private readonly Stage[] nonCombatZone = Conditionals.nonCombatStages;
+        private readonly Dictionary<string, Int32> subWeapon = Conditionals.SubWeapon;
+
+        private Weapon? subWeaponObj;
+
+        private bool IsInNonCombatArea()
+        {
+            return nonCombatZone.Contains(Area.CurrentStage);
+        }
+
+        public PluginData Initialize()
+        {
+            return new PluginData
+            {
+                OnUpdate = true
+            };
+        }
+
+        public void OnLoad()
+        {
+            Log.Info("Loaded Hide Weapon");
+        }
+
+        public void OnUpdate(float dt)
+        {
+            var weapon = Player.MainPlayer?.CurrentWeapon;          
+            if (weapon is not null)
+            {
+                var weaponType = weapon.GetDti().Name;
+                if (subWeapon.ContainsKey(weaponType))
+                {
+                    subWeaponObj = weapon.GetObject<Weapon>(subWeapon[weaponType]);
+                }                
+                if (IsInNonCombatArea())
+                {
+                    // Making the weapon invisible
+                    weapon.GetRef<uint>(0x14) &= 0xFFFFFFFC; // Clear the lowest 2 bits of the integer at offset 0x14
+                    if (subWeaponObj != null)
+                    {
+                        subWeaponObj.GetRef<uint>(0x14) &= 0xFFFFFFFC;
+                    }                    
+                }
+                else
+                {
+                    // Making the weapon visible
+                    weapon.GetRef<uint>(0x14) |= 3; // Set the lowest 2 bits of the integer at offset 0x14
+                    if (subWeaponObj != null)
+                    {
+                        subWeaponObj.GetRef<uint>(0x14) |= 3;
+                    }       
+                }
+            }
+        }
+    }
+}
